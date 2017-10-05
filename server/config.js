eb16909d00cb42747a060a82af766cf40a7fe3b7
Aidan Dempster <aidan.dempster@gmail.com>
2017-10-05 10:03:21 -0400

    Added capability to connect to a mysql server.
    Added comments

--- server/config.js
+++ server/config.js
@@ -1,1 +1,10 @@
+//Defines the port the server should run on
 export const PORT = process.env.PORT || 3001;
+
+//Defines default options for database if not defined by environment variables
+export const dbOptions = {
+  host     : "localhost",
+  user     : "root",
+  password : "root",
+  port     : "3306"
+}
--- server/server.js
+++ server/server.js
@@ -1,12 +1,29 @@
 import express from 'express';
-import * as config from './config'
+import * as config from './config';
+import mysql from 'mysql';
 const app = express();
+
+const connection = mysql.createConnection({
+  host     : process.env.RDS_HOSTNAME || config.dbOptions.host,
+  user     : process.env.RDS_USERNAME || config.dbOptions.user,
+  password : process.env.RDS_PASSWORD || config.dbOptions.password,
+  port     : process.env.RDS_PORT || config.dbOptions.port
+});
+
+connection.connect((err) => {
+  if(err){
+    console.error('Failed to connect to database: '+err.stack);
+    return;
+  }
+
+  console.log("Established connection to database");
+  //Only start the server if the connection is successful
+  const listener = app.listen(config.PORT, () => {
+    console.log(`Find at: http://localhost:${listener.address().port}`);
+  });
+});
 
 app.get("/:id", (req, res) => {
   const params = req.params;
   res.send(`You said id: ${params.id}`);
-});
-
-const listener = app.listen(config.PORT, () => {
-  console.log(`Find at: http://localhost:${listener.address().port}`);
 });
